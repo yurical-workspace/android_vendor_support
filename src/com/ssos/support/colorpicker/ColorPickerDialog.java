8d4369d998f3a04dd80030c3cef879923b44ec20
Anushek Prasal <anushekprasal@gmail.com>
2020-07-17 23:35:25 +0530

    support: ColorPicker: Add haptic feedback to some buttons
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- src/com/ssos/support/colorpicker/ColorPickerDialog.java
+++ src/com/ssos/support/colorpicker/ColorPickerDialog.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2010 Daniel Nilsson
  * Copyright (C) 2013 Slimroms
+ * Copyright (C) 2020 Havoc-OS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +22,9 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
@@ -31,6 +34,7 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import com.ssos.support.R;
+import androidx.annotation.NonNull;
 
 public class ColorPickerDialog extends AlertDialog implements ColorPickerView.OnColorChangedListener, View.OnClickListener, View.OnKeyListener {
 
@@ -41,6 +45,9 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
 
     private OnColorChangedListener mListener;
 
+    private final Context mContext;
+    private final Vibrator mVibrator;
+
     public interface OnColorChangedListener {
         void onColorChanged(int color);
     }
@@ -49,6 +56,9 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
         super(context);
 
         init(initialColor);
+
+        mContext = context;
+        mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
     private void init(int color) {
@@ -145,6 +155,7 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
                 mListener.onColorChanged(mNewColor.getColor());
             }
         }
+        doHapticFeedback();
         dismiss();
     }
 
@@ -163,5 +174,14 @@ public class ColorPickerDialog extends AlertDialog implements ColorPickerView.On
         super.onRestoreInstanceState(savedInstanceState);
         mOldColor.setColor(savedInstanceState.getInt("old_color"));
         mColorPicker.setColor(savedInstanceState.getInt("new_color"), true);
+    }
+
+    private void doHapticFeedback() {
+        final boolean hapticEnabled = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.HAPTIC_FEEDBACK_ENABLED, 1) != 0;
+
+        if (hapticEnabled) {
+            mVibrator.vibrate(VibrationEffect.get(VibrationEffect.EFFECT_CLICK));
+        }
     }
 }
